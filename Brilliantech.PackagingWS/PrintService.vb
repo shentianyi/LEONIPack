e08808eecf0a7e639a5ef33e61d360877e0fbade
tianyi <tianyi.shen@brilliantech-solution.com>
2013-01-16 20:48:50 +0800

    Open the partContainerLabel function

--- Brilliantech.PackagingWS/PrintService.vb
+++ Brilliantech.PackagingWS/PrintService.vb
@@ -67,8 +67,11 @@ Public Class PrintService
                 LabelData.Add("custPartnr", package.Part.CustomerPartNr)
                 Dim records As RecordSet = New RecordSet
                 records.Add(LabelData)
-                Dim template As String = myConfig.Configs("LabelTemplate").Get("CloseLabelFile")
-                Dim task As PrintTask = New PrintTask With {.Config = New ReportGenConfig With {.NumberOfCopies = piece, .Template = template}, .DataSet = records}
+
+
+                Dim templateName As String = myConfig.Configs("LabelTemplateName").Get("CloseLabelFile")
+                Dim containerLabel As PartContainerLabel = package.Part.PartContainerLabels.FirstOrDefault(Function(c) c.LabelTemplateName = templateName)
+                Dim task As PrintTask = New PrintTask With {.Config = New ReportGenConfig With {.NumberOfCopies = piece, .Template = containerLabel.LabelTeplateFile}, .DataSet = records}
                 Dim tasks As List(Of PrintTask) = New List(Of PrintTask)
                 tasks.Add(task)
                 result.PrintTask = tasks
